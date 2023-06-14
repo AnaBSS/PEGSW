cb4ace5270b93e49b6f119d832f4973901b4758e
Ana Beatriz <100586167+AnaBSS@users.noreply.github.com>
2023-06-14 01:55:19 -0300

    Delete persistence directory

--- persistence/IPessoaDAO.java
+++
@@ -1,9 +0,0 @@
-package persistence;
-
-import java.sql.SQLException;
-
-import model.Pessoa;
-
-public interface IPessoaDAO {
-	public void realizarLogin(Pessoa p) throws ClassNotFoundException, SQLException;
-}
